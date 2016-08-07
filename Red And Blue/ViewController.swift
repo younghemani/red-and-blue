542ce42b6e33e1afb0a622b0f335b28eb86e3769
Ali Hemani <ahemani888@gmail.com>
2016-08-07 11:21:45 -0500

    Initial Commit

---
+++ Red And Blue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Red And Blue
+//
+//  Created by Ali Hemani on 8/7/16.
+//  Copyright © 2016 Ali Hemani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
